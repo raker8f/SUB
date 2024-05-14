3807f5514c43ed5c56adfe04fed7a0b77d756781
raker8f <135430943+raker8f@users.noreply.github.com>
2024-05-14 02:11:54 +0000

    simulate and get score

--- src/test/java/Fall2023Main.java
+++ src/test/java/Fall2023Main.java
@@ -1,19 +1,41 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.simulate.GameResult;
 import com.google.common.io.Files;
 
 public class Fall2023Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        int[] finalScores = getFinalScores();
+        System.out.println(Arrays.toString(finalScores));
+        writeScoresToFile(finalScores);
+    }
 
+    private static int[] getFinalScores() throws IOException, InterruptedException {
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
-//        gameRunner.setSeed(-8358938852454912011l);
         gameRunner.addAgent("python3 config/Boss.py", "TestBoss_1");
         gameRunner.addAgent("python3 config/Boss.py", "TestBoss_2");
-        gameRunner.setLeagueLevel(4);
-        
-        gameRunner.start();
+        gameRunner.setLeagueLevel(1);
+        GameResult gameResult = gameRunner.simulate();
+
+        int[] finalScores = new int[2];
+        for (int i = 0; i < gameResult.scores.size(); i++) {
+            finalScores[i] = gameResult.scores.get(i);
+        }
+        return finalScores;
+    }
+
+    
+    private static void writeScoresToFile(int[] scores) throws IOException {
+        FileWriter writer = new FileWriter("scores.txt");
+        for (int i = 0; i < scores.length; i++) {
+            writer.write(String.valueOf(scores[i]));
+            writer.write("\n"); // 每個分數一行
+        }
+        writer.close();
     }
 
     private static String compile(String botFile) throws IOException, InterruptedException {
